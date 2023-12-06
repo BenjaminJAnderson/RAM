8bc60af39386672a90435e819ba357dc476f9d50
BenjaminJAnderson <thebenjaminjanderson@gmail.com>
2023-12-06 22:41:37 +0000

    improvement/simplification on the enhance function, now want to take these pixels as a point cloud and then make a best fit curve around it

--- TemplateCreator.py
+++ TemplateCreator.py
@@ -13,75 +13,53 @@ def load_image(file_path):
 		print("Unable to load image")
 		return None
 
-def enhance_contrast(image):
+def enhance_image(image, threshold=128):
 	# Enhance the contrast of the image
-	enhancer = ImageEnhance.Contrast(image)
+	grayscale_image = image.convert('L')
+	enhancer = ImageEnhance.Contrast(grayscale_image)
 	enhanced_image = enhancer.enhance(5)  # Adjust the factor for desired contrast enhancement
-	return enhanced_image
+	threshold_image = enhanced_image.point(lambda p: 0 if p < threshold else 255, '1')
+	dilated_image = threshold_image.filter(ImageFilter.MaxFilter(size=3))
 
-def convert_to_monotone(image, threshold=128):
-	grayscale_image = image.convert("L")  # Convert to grayscale
-	# Threshold the image to create a monotone image
-	monotone_image = grayscale_image.point(lambda p: 0 if p < threshold else 255, '1')
-	return monotone_image
+	plt.subplot(121),plt.imshow(image,cmap = 'gray')
+	plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+	plt.subplot(122),plt.imshow(dilated_image,cmap = 'gray')
+	plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+	plt.show()
+	return enhanced_image
 
 def vectorize_image(image):
 	img = cv2.imread(image)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-	edges = cv2.Canny(blurred, 50, 150)
+	filtered = cv2.bilateralFilter(gray, 15, 75, 75)
+	blur = cv2.GaussianBlur(filtered, (9, 9), 0)
+	kernel = np.ones((10, 10), np.uint8) # Adjusting this helps
+	morph = cv2.morphologyEx(blur, cv2.MORPH_CLOSE, kernel)
+	edges = cv2.Canny(morph, 50, 150)
+	kernel = np.ones((10, 10), np.uint8)
+	dilated_edges = cv2.dilate(edges, kernel, iterations=1)
 	contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	
 	smoothed_image = np.zeros_like(img)
 
 	for contour in contours:
-		epsilon = 0.01 * cv2.arcLength(contour, True)
+		epsilon = 0.001 * cv2.arcLength(contour, True)
 		approx = cv2.approxPolyDP(contour, epsilon, True)
 		cv2.drawContours(smoothed_image, [approx], -1, (0, 255, 0), 2)
 
 	plt.subplot(121),plt.imshow(img,cmap = 'gray')
 	plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-	plt.subplot(122),plt.imshow(smoothed_image,cmap = 'gray')
+	plt.subplot(122),plt.imshow(dilated_edges,cmap = 'gray')
 	plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 	plt.show()
 
-	return image
+pic = load_image("/home/benjamin/Documents/Projects/RAM/right.jpg")
 
-# Function to convert the loaded image into a vector format
-# def convert_to_vector(image):
-	# Code to process the image and convert it into a vector format using edge detection or other techniques
-
-# # Function to extract the outline of the foot from the vector image
-# def extract_foot_outline(vector_image):
-# 	# Code to analyze the vector image and extract the foot outline, possibly using image processing algorithms
-
-# # Function to smooth the extracted foot outline
-# def smooth_outline(foot_outline):
-# 	# Code to apply a smoothing algorithm to the foot outline, removing jagged edges or imperfections
-
-# # Function to conform the outline to a more aesthetic shape
-# def conform_to_aesthetic_shape(smoothed_outline):
-# 	# Code to reshape the outline to a more aesthetic form, using predefined templates or mathematical transformations
-
-# # Function to generate the final sandal shape
-# def generate_sandal_shape(aesthetic_outline, sole_shape):
-# 	# Code to combine the aesthetic foot outline with a sole shape to create the final sandal shape
-
-# # Function to output/save/display the resulting sandal shape
-# def output_result(sandal_shape):
-# 	# Code to save or display the resulting sandal shape for the user
-
-pic = load_image("/home/benjamin/Documents/Projects/RAM/left.jpg")
-
-epic = enhance_contrast(pic)
-
-eepic = convert_to_monotone(epic)
+epic = enhance_image(pic)
 
 
 epic.save("cont.jpg")
-eepic.save("mono.jpg")
 
-
-vector = vectorize_image("mono.jpg")
+# vector = vectorize_image("mono.jpg")
 # vector.save("vect.jpg")
 
