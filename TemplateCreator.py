022757e56f49f053ee04edaef05323aea482a9b7
BenjaminJAnderson <thebenjaminjanderson@gmail.com>
2023-12-07 03:56:53 +0000

    getting closer with the np.squeeze

--- TemplateCreator.py
+++ TemplateCreator.py
@@ -37,21 +37,19 @@ def vectorize_image(image):
 	epsilon = 0.01 * cv2.arcLength(contours[0], True)
 	poly_contour = cv2.approxPolyDP(contours[0], epsilon, True)
 
-	# polyArr = list(poly_contour)
 	
-	# smooth_contours = []
-	# for points in poly_contour[0]:
-	# 		t = np.arange(len(points))
-	# 		poly_degree = 3  # Adjust the polynomial degree as needed
-	# 		coeffs = np.polyfit(t, points[:, 0], poly_degree), np.polyfit(t, points[:, 1], poly_degree)
-	# 		t_new = np.linspace(0, len(points) - 1, 100)
-	# 		x_smooth = np.polyval(coeffs[0], t_new)
-	# 		y_smooth = np.polyval(coeffs[1], t_new)
-	# 		smooth_contours.append(np.column_stack((x_smooth, y_smooth)).astype(np.int32))
+	smooth_contours = []
+	for points in np.squeeze(poly_contour):
+			t = np.arange(len(points))
+			poly_degree = 3  # Adjust the polynomial degree as needed
+			coeffs = np.polyfit(t, points[:, 0], poly_degree), np.polyfit(t, points[:, 1], poly_degree)
+			t_new = np.linspace(0, len(points) - 1, 100)
+			x_smooth = np.polyval(coeffs[0], t_new)
+			y_smooth = np.polyval(coeffs[1], t_new)
+			smooth_contours.append(np.column_stack((x_smooth, y_smooth)).astype(np.int32))
 
-	print(poly_contour)
 	contour_image = np.zeros_like(img)
-	contour_image = cv2.drawContours(img, [poly_contour], -1, (0,255,0), 10)
+	contour_image = cv2.drawContours(img, smooth_contours, -1, (0,255,0), 10)
 
     
 
