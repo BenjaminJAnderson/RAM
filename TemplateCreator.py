208e8b91a3fe8628ac5a3fdbfa2d596d774d4661
BenjaminJAnderson <thebenjaminjanderson@gmail.com>
2023-12-07 08:52:42 +0000

    cleaning up the code

--- TemplateCreator.py
+++ TemplateCreator.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageEnhance
 import cv2
 import numpy as np
 from scipy.interpolate import splprep, splev
@@ -15,22 +15,48 @@ def load_image(file_path):
 		print("Unable to load image")
 		return None
 
-def enhance_image(image):
+def enhance_hole(image):
+	enhancer = ImageEnhance.Contrast(image)
+	enhanced_image = enhancer.enhance(3)
+
+	width, height = image.size
+	for x in range(width):
+		for y in range(height):
+			pixel = enhanced_image.getpixel((x, y))
+			# Check if the pixel is not black
+			if pixel != (0, 0, 0):
+				# If it's not black, set it to white
+				enhanced_image.putpixel((x, y), (255, 255, 255))  # Set as white
+
+	grayscale_image = enhanced_image.convert('L')
+	threshold_image = grayscale_image.point(lambda p: 0 if p < 255 else 255, '1')
+	dilated_image = threshold_image.filter(ImageFilter.MaxFilter(size=5))
+	compatible_image = dilated_image.convert('RGB')
+	blurred_image = compatible_image.filter(ImageFilter.GaussianBlur(radius=10))
+
+	# plt.subplot(121),plt.imshow(image,cmap = 'gray')
+	# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+	# plt.subplot(122),plt.imshow(blurred_image,cmap = 'gray')
+	# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+	# plt.show()
+
+
+	return blurred_image
+
+def img2Outline(image):
 	grayscale_image = image.convert('L')
 	threshold_image = grayscale_image.point(lambda p: 0 if p < 255 else 255, '1')
 	dilated_image = threshold_image.filter(ImageFilter.MaxFilter(size=5))
 	compatible_image = dilated_image.convert('RGB')
 	blurred_image = compatible_image.filter(ImageFilter.GaussianBlur(radius=10))
 
-	# plt.subplot(121),plt.imshow(threshold_image,cmap = 'gray')
-	# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-	# plt.subplot(122),plt.imshow(blurred_image,cmap = 'gray')
-	# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-	# plt.show()
-	return blurred_image
-
-def img2Array(image):
-	img = cv2.imread(image)
+	plt.subplot(121),plt.imshow(image,cmap = 'gray')
+	plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+	plt.subplot(122),plt.imshow(blurred_image,cmap = 'gray')
+	plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+	plt.show()
+	blurred_image.save("enhanced_outline.jpg")
+	img = cv2.imread("enhanced_outline.jpg")
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	ret, im = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
 	contours, hierarchy  = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -53,74 +79,71 @@ def img2Array(image):
 	return x_new, y_new
 
 
-outline = load_image("/home/benjamin/Documents/Projects/RAM/right.jpg")
+if __name__ == "__main__":
+	drawing = load_image("left.jpg")
 
-enhanced_outline = enhance_image(outline)
-enhanced_outline.save("enhanced.jpg")
-x,y = img2Array("/home/benjamin/Documents/Projects/RAM/enhanced.jpg")
+	x,y = img2Outline(drawing)
+	# enhanced_hole = enhance_hole(drawing)
 
-N_index = np.argmin(y)
-NX, NY = x[N_index], y[N_index]
+	N_index = np.argmin(y)
+	E_index = np.argmax(x)
+	S_index = np.argmax(y)
+	W_index = np.argmin(x)
+	NX, NY = x[N_index], y[N_index]
+	EX, EY = x[E_index], y[E_index]
+	SX, SY = x[S_index], y[S_index]
+	WX, WY = x[W_index], y[W_index]
 
-E_index = np.argmax(x)
-EX, EY = x[E_index], y[E_index]
+	#A4 conversion
+	A4_x, A4_y = 2480, 3508
+	pix2mmX, pix2mmY= 210/A4_x, 297/A4_y
 
-S_index = np.argmax(y)
-SX, SY= x[S_index], y[S_index]
+	width = np.around(np.sqrt((EX - WX)**2 + (EY - WY)**2))
+	height = np.around(np.sqrt((NX - SX)**2 + (NY - SY)**2))
 
-W_index = np.argmin(x)
-WX, WY = x[W_index], y[W_index]
+	#Bottom holes
+	BottomY = SY - (height * 0.20)
+	tolerance = 4
+	PixelGap = 75
+	indices_close = [i for i, y_val in enumerate(y) if abs(y_val - BottomY) < tolerance]
+	BLHole = x[indices_close[0]] + PixelGap
+	BRHole = x[indices_close[-1]] - PixelGap
 
-#A4 conversion
-A4_x, A4_y = 2480, 3508
-pix2mmX, pix2mmY= 210/A4_x, 297/A4_y
-
-width = np.around(np.sqrt((EX - WX)**2 + (EY - WY)**2))
-height = np.around(np.sqrt((NX - SX)**2 + (NY - SY)**2))
-
-#Bottom holes
-BottomY = SY - (height * 0.20)
-tolerance = 4
-PixelGap = 75
-indices_close = [i for i, y_val in enumerate(y) if abs(y_val - BottomY) < tolerance]
-BLHole = x[indices_close[0]] + PixelGap
-BRHole = x[indices_close[-1]] - PixelGap
-
-#Bottom holes
-MidY = SY - (height * 0.45)
-indices_close = [i for i, y_val in enumerate(y) if abs(y_val - MidY) < tolerance]
-MLHole = x[indices_close[0]] + PixelGap
-MRHole = x[indices_close[-1]] - PixelGap
+	#Bottom holes
+	MidY = SY - (height * 0.45)
+	indices_close = [i for i, y_val in enumerate(y) if abs(y_val - MidY) < tolerance]
+	MLHole = x[indices_close[0]] + PixelGap
+	MRHole = x[indices_close[-1]] - PixelGap
 
 
-##################### SETTINGS #####################
-fig, ax = plt.subplots(figsize=(8.26772, 11.6929)) # A4 Paper
-fig.tight_layout()
-ax.axis('off')
+	##################### SETTINGS #####################
+	fig, ax = plt.subplots(figsize=(8.26772, 11.6929)) # A4 Paper
+	fig.tight_layout()
+	ax.axis('off')
 
-##################### IMAGE #####################
-ax.imshow(outline)
+	##################### IMAGE #####################
+	ax.imshow(drawing)
 
-##################### HEIGHT & WIDTH INFO #####################
-ax.plot([NX, SX], [NY, SY], 'g--')
-ax.plot([EX, WX], [EY, WY], 'r--')
-ax.text(50, 150, f"Width = {width * pix2mmX}mm", fontsize=7)
-ax.text(50, 300, f"Height = {height * pix2mmY}mm", fontsize=7)
+	##################### HEIGHT & WIDTH INFO #####################
+	ax.plot([NX, SX], [NY, SY], 'g--')
+	ax.plot([EX, WX], [EY, WY], 'r--')
+	ax.text(50, 150, f"Width = {width * pix2mmX}mm", fontsize=7)
+	ax.text(50, 300, f"Height = {height * pix2mmY}mm", fontsize=7)
 
 
-##################### OUTLINE & STITCHLINE #####################
-ax.plot(x, y, linestyle='solid', linewidth=15, color='black')
-ax.plot(x, y, linestyle='dotted', linewidth=2, color='white')
+	##################### OUTLINE & STITCHLINE #####################
+	ax.plot(x, y, linestyle='solid', linewidth=15, color='black')
+	ax.plot(x, y, linestyle='dotted', linewidth=2, color='white')
 
-##################### BOTTOM HOLES #####################
-ax.plot([BLHole, BRHole],[BottomY, BottomY], "ro", markersize=10)
+	##################### BOTTOM HOLES #####################
+	ax.plot([BLHole, BRHole],[BottomY, BottomY], "ro", markersize=10)
 
-##################### MIDDLE HOLES #####################
-ax.plot([MLHole, MRHole],[MidY, MidY], "ro", markersize=10)
+	##################### MIDDLE HOLES #####################
+	ax.plot([MLHole, MRHole],[MidY, MidY], "ro", markersize=10)
 
 
 
-fig.savefig('modified_a4_figure.png', dpi=300, bbox_inches='tight')  # Set dpi as needed (300 is standard for printing)
-plt.show()
+	fig.savefig('modified_a4_figure.png', dpi=300, bbox_inches='tight')  # Set dpi as needed (300 is standard for printing)
+	plt.show()
 
 
